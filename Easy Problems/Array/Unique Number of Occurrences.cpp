abbaeb01370095c6f0e5a6e82ef7cf51af819f2d
vansh kumar <vanshchaudhary2404@gmail.com>
2025-05-19 22:46:12 +0530

    Create Unique Number of Occurrences.cpp

---
+++ Easy Problems/Array/Unique Number of Occurrences.cpp
@@ -0,0 +1,26 @@
+//Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int freq[2001] ={0};
+
+        for(int i=0; i<arr.size(); i++){
+            freq[arr[i]+ 1000]++;
+        }
+        vector<int> countList;
+        for(int i=0; i<2001; i++){
+            if( freq[i] != 0){
+                countList.push_back(freq[i]);
+            }
+        }
+        sort(countList.begin() , countList.end());
+
+        for(int i=1; i<countList.size(); i++){
+            if( countList[i] == countList[i-1]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
