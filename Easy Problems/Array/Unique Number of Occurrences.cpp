8fa4fecc58420c97276dfa0d2bc49c80a6d4ad8b
vansh kumar <vanshchaudhary2404@gmail.com>
2025-05-24 20:31:56 +0530

    Update Unique Number of Occurrences.cpp

--- Easy Problems/Array/Unique Number of Occurrences.cpp
+++ Easy Problems/Array/Unique Number of Occurrences.cpp
@@ -1,3 +1,4 @@
+//LEETCODE EASY PROBLEM
 //Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
 class Solution {
 public:
