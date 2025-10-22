a7b90af3a8c044a0d87839a71568f165ad681745
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-10-22 16:16:42 +0530

    Update count Inversion.cpp

--- Recursion/count Inversion.cpp
+++ Recursion/count Inversion.cpp
@@ -9,6 +9,58 @@ Conditions to be satisfy
 1. i < j.
 2. arr[i] > arr[j].
 */
+//GFG Solution
+class Solution {
+  public:
+    int merge( vector<int>&arr, int st ,int mid , int end){
+          vector<int> temp;
+          int i = st , j = mid+1;
+          int invCount = 0;
+          while( i <= mid && j <= end){
+            if( arr[i] <= arr[j]){
+              temp.push_back(arr[i]);
+              i++;
+            } else{
+              temp.push_back(arr[j]);
+              j++;
+              invCount += (mid-i+1);
+            }
+          } 
+          while( i<=mid){
+            temp.push_back(arr[i]);
+            i++;
+          }
+          while( j <= end){
+            temp.push_back(arr[j]);
+            j++;
+          }
+          for(int idx =0; idx<temp.size(); idx++){
+            arr[idx+st] = temp[idx];
+          }
+          return invCount;
+    }
+
+    int mergeSort(vector<int> &arr , int st , int end){
+          if(st < end){
+            int mid = st + (end-st)/2;
+        
+            int leftInvCount = mergeSort(arr , st , mid);//Left half
+            int rightInvCount = mergeSort(arr, mid+1, end);//right half
+        
+            int invCount = merge(arr , st , mid , end);
+            
+            return leftInvCount + rightInvCount + invCount;
+          }
+          return 0;
+    }
+    int inversionCount(vector<int> &arr) {
+        // Code Here
+        return mergeSort(arr , 0 , arr.size()-1);
+
+    }
+}
+
+//VSCODE SOLUTION
 #include<iostream>
 #include<vector>
 using namespace std;
