42064cc47e72cc9439da8dc2ce0707d4a69b132b
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-10-25 21:19:06 +0530

    Create Allocate Minimum Pages.cpp

---
+++ Binary Search/Allocate Minimum Pages.cpp
@@ -0,0 +1,5 @@
+/* Author : Vansh Kumar
+  Date : 2025-09-30
+  Description: GFG problem- Allocate Minimum Pages
+  link - https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+*/
