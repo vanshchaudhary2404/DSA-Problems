6e170622a04ffd1f56cdf1fc497c763f83342c32
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-10-29 00:42:22 +0530

    Update Unique number of Occurrences.cpp

--- Array/Unique number of Occurrences.cpp
+++ Array/Unique number of Occurrences.cpp
@@ -5,25 +5,45 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int freq[2001] ={0};
+        // int freq[2001] ={0};
 
-        for(int i=0; i<arr.size(); i++){
-            freq[arr[i]+ 1000]++;
-        }
-        vector<int> countList;
-        for(int i=0; i<2001; i++){
-            if( freq[i] != 0){
-                countList.push_back(freq[i]);
+        // for(int i=0; i<arr.size(); i++){
+        //     freq[arr[i]+ 1000]++;
+        // }
+        // vector<int> countList;
+        // for(int i=0; i<2001; i++){
+        //     if( freq[i] != 0){
+        //         countList.push_back(freq[i]);
+        //     }
+        // }
+        // sort(countList.begin() , countList.end());
+
+        // for(int i=1; i<countList.size(); i++){
+        //     if( countList[i] == countList[i-1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int i = 0;
+        sort(arr.begin(), arr.end());
+        vector<int> ans;
+        while (i < arr.size()) {
+            int count = 1;
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
             }
+            ans.push_back(count);
+            i = i + count;
         }
-        sort(countList.begin() , countList.end());
-
-        for(int i=1; i<countList.size(); i++){
-            if( countList[i] == countList[i-1]){
+        sort(ans.begin(), ans.end());
+        for (int i = 0; i < ans.size() - 1; i++) {
+            if (ans[i] == ans[i + 1]) {
                 return false;
             }
         }
-        
         return true;
     }
 };
