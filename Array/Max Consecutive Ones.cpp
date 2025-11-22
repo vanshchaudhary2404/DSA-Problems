66a773870a256300043a423c3a524807565ad2bb
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-11-23 00:58:09 +0530

    Add solution for Max Consecutive Ones problem
    Implemented a solution to find the maximum number of consecutive 1s in a binary array.

--- Array/Max Consecutive Ones.cpp
+++ Array/Max Consecutive Ones.cpp
@@ -1,1 +1,21 @@
-/*
+/* Author : Vansh Kumar
+  Date : 2025-10-15
+  Description: leetcode- 485. Max Consecutive Ones
+*/
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0 , maxi =0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == 1){
+                cnt++;
+                maxi = max( maxi , cnt);
+            } else{
+                cnt =0;
+            }
+        }
+        return maxi;
+    }
+};
