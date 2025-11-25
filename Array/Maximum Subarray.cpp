21fe203fa2c59eb2a0b4efc8ec5469acb69ce514
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-11-25 14:04:58 +0530

    Update Maximum Subarray.cpp

--- Array/Maximum Subarray.cpp
+++ Array/Maximum Subarray.cpp
@@ -42,3 +42,27 @@ public:
         return maxSum;
     }
 };
+
+
+
+"Advance version fro printing the subarray having max subarray sum"
+  int currSum =0 , maxSum = INT_MIN;
+        int ansStart = -1 , ansEnd = -1;
+        for(int i=0; i<nums.size(); i++){
+            if(currSum == 0) 
+                start = i;
+            currSum += nums[i];
+            if( currSum > maxSum){
+              maxSum = currSum;
+              ansStart = start;
+              ansEnd = i;
+            }
+            
+            if( currSum < 0) currSum = 0;
+        }
+        cout << "Max subarray: ";
+        for (int i = ansStart; i <= ansEnd; i++)
+           cout << nums[i] << " ";
+            cout << endl;
+        return maxSum;
+    }
