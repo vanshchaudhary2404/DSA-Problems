ccdbf557aa5364f31f1c8e1ffaaca7fb714d03ee
Vansh Kumar <vanshchaudhary2404@gmail.com>
2025-11-25 13:56:24 +0530

    Update Maximum Subarray.cpp

--- Array/Maximum Subarray.cpp
+++ Array/Maximum Subarray.cpp
@@ -4,6 +4,33 @@
 */
 class Solution {
 public:
+        //Brtue force T.C = O(n^3)
+        /*int maxSum = INT_MIN; 
+        int n = nums.size();
+        for(int i =0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int currSum =0; 
+                for(int k=i; k<=j; k++){
+                    currSum += nums[k];
+                    maxSum = max( currSum , maxSum);
+                }
+            }
+        }
+        return maxSum;*/
+
+        //Better Approach T.C =O(n^2)
+        /*int maxSum = INT_MIN; 
+        int n = nums.size();
+        for(int st =0; st<n; st++){
+            int currSum =0; 
+            for(int end=st; end<n; end++){
+                currSum += nums[end];
+                maxSum = max( currSum , maxSum);
+            }
+        }
+        return maxSum;*/
+
+    //OPtinmal Approach : T.C = O(n) , S.C = O(1)
     int maxSubArray(vector<int>& nums) {
         int currSum =0 , maxSum = INT_MIN;
         for(int val : nums){
