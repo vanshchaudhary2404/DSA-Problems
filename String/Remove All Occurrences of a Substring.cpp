c4160d962399d33d279245540e3afa3b2b4ca302
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-01-26 23:20:44 +0530

    Update Remove All Occurrences of a Substring.cpp

--- String/Remove All Occurrences of a Substring.cpp
+++ String/Remove All Occurrences of a Substring.cpp
@@ -1,1 +1,13 @@
-
+/* Author : Vansh Kumar
+  Date : 2026-01-26
+  Description: Leetcode problem-1910
+*/
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while( s.length() > 0 && s.find(part) < s.length()){
+            s.erase(s.find(part) , part.length());
+        }
+        return s;
+    }
+};
