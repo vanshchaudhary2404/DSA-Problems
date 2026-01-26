e38d3566023cb7a1a94cdc4871951ea4def869b9
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-01-26 23:27:56 +0530

    Create String Comparission.cpp

---
+++ String/String Comparission.cpp
@@ -0,0 +1,33 @@
+/* Author : Vansh Kumar
+  Date : 2026-01-26
+  Description: Leetcode problem-443
+*/
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int idx = 0;
+
+        for(int i=0; i<n; i++){
+            int count =0; 
+            char ch  = chars[i];
+
+            while( i< n && chars[i] == ch){
+                count ++;
+                i++;
+            } 
+
+            if( count == 1){
+                chars[idx++] = ch;
+            } else {
+                chars[idx++] = ch;
+                string str = to_string(count);
+                for( char dig : str){
+                    chars[idx++] = dig;
+                }
+            }
+            i--;
+        }
+        return idx;
+    }
+};
