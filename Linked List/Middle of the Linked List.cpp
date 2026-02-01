a030aa6eb80f94299524c789e640d621602488ff
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-02-01 16:24:02 +0530

    Create Middle of the Linked List.cpp

---
+++ Linked List/Middle of the Linked List.cpp
@@ -0,0 +1,28 @@
+/* Author : Vansh Kumar
+  Date : 2025-01-02
+  Description: Leetcode problem-876
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while( fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
