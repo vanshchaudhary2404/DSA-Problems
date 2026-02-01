4474e906c7a1b072c63376ac9279f80419a5b899
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-02-01 16:28:21 +0530

    Create Swap Nodes in Pairs.cpp

---
+++ Linked List/Swap Nodes in Pairs.cpp
@@ -0,0 +1,44 @@
+/* Author : Vansh Kumar
+  Date : 2025-01-02
+  Description: Leetcode problem-24
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if( head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* first = head;
+        ListNode* sec = head->next;
+        ListNode* prev = NULL;
+        while( first != NULL && sec !=NULL){
+            ListNode* third = sec->next;
+            sec->next = first;
+            first->next = third;
+            if( prev != NULL){
+                prev->next = sec;
+            }else{
+                head = sec;
+            }
+            //Update pointers for next pairs;
+            prev = first;
+            first = third;
+            if(third != NULL){
+                sec = third->next;
+            }else{
+                sec = NULL;
+            }
+        }
+        return head;
+    }
+};
