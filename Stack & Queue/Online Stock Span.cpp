dda9004adad444975d2496d5df8b326ed98cffbd
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-02-08 20:45:39 +0530

    Add StockSpanner class for stock span calculation
    Implement StockSpanner class for calculating stock span.

---
+++ Stack & Queue/Online Stock Span.cpp
@@ -0,0 +1,29 @@
+/* Author : Vansh Kumar
+  Date : 2025-08-02
+  Description: Leetcode problem-901.
+*/
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    stack<pair<int,int>> st;
+    int next(int price) {   
+        int span = 1;
+
+        // pop all smaller/equal prices
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+
+        st.push({price, span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
