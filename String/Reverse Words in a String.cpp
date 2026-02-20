6d549113759f0dfddd876be01e24fb1de2146efe
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-02-20 12:03:00 +0530

    Update Reverse Words in a String.cpp

--- String/Reverse Words in a String.cpp
+++ String/Reverse Words in a String.cpp
@@ -1,6 +1,6 @@
 /* Author : Vansh Kumar
   Date : 2026-01-26
-  Description: Leetcode problem-152
+  Description: Leetcode problem-152 Reverse Words in a String
 */
 
 class Solution {
@@ -13,16 +13,18 @@ public:
 
         for(int i=0; i<n; i++){
             string word ="";
+            //store the single reversed word in word string
             while( i < n && s[i] != ' '){
                 word += s[i];
                 i++;
             }
+            //reverse that word string to get correct word order
             reverse( begin(word), end(word));
 
             if( word.length() > 0){
                 ans += " "+ word;
             }
         }
-        return ans.substr(1);
+        return ans.substr(1);//to remove the leading space from ans;
     }
 };
