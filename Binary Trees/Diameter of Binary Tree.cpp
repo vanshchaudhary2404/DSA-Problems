7d9d3cd05b3f57b27fb777e49bfe5f3db387e199
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-03-17 00:12:49 +0530

    Create Diameter of Binary Tree.cpp

---
+++ Binary Trees/Diameter of Binary Tree.cpp
@@ -0,0 +1,36 @@
+/* Author : Vansh Kumar
+  Date : 2025-16-03
+  Description: Leetcode problem-543. Diameter of Binary Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root , diameter);
+        return diameter;
+    }
+
+    int height( TreeNode* root , int& diameter){
+        if(!root){
+            return 0;
+        }
+        int lh = height(root->left , diameter);
+        int rh = height(root->right , diameter);
+
+        diameter = max(diameter , lh+rh);
+
+        return 1+max(lh , rh);
+    }
+};
