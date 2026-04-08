577f5f14778eec34af1175c872733ed74063969f
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-04-09 00:22:25 +0530

    Create Two Sum IV - Input is a BST.cpp

---
+++ Binary Search Tree/Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,74 @@
+/* Author : Vansh Kumar
+  Date : 2025-09-04
+  Description: Leetcode problem-653. Two Sum IV - Input is a BST
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ class BSTIterator {
+private: 
+    stack<TreeNode*> myStack;
+    // reverse -> true -> before
+    // reverse -> false -> next
+    bool reverse = true;
+public:
+    BSTIterator(TreeNode* root , bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* tempNode = myStack.top();
+        myStack.pop();
+        if(!reverse) pushAll(tempNode->right);
+        else pushAll(tempNode->left);
+        return tempNode->val;
+    }
+    
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+private:
+    void pushAll(TreeNode* root){
+        while( root != NULL){
+            myStack.push(root);
+            if(reverse == true){
+                root = root->right;
+            }else{
+                root = root->left;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+
+        //next 
+        BSTIterator l(root , false);
+        //before
+        BSTIterator r(root , true);
+
+        int i = l.next();
+        int j= r.next();//r.before
+
+        while(i<j){
+            if(i+j == k ) return true;
+            else if(i+j <k) i = l.next();
+            else j = r.next();
+        }
+
+        return false;
+    }
+};653. Two Sum IV - Input is a BST
