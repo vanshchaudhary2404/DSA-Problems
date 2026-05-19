be3bfaba163089684f6da789fa7c76fecdee74ee
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-05-19 18:52:40 +0530

    Update 2540. Minimum Common Value.cpp

--- Daily Challenge/2540. Minimum Common Value.cpp
+++ Daily Challenge/2540. Minimum Common Value.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        //stor ele of num1 in set
+        unordered_set<int> s(begin(nums1) , end(nums1));
+        int miniCommon = INT_MAX;
+
+        for(int x: nums2){
+            if(s.count(x)){
+                miniCommon = min(miniCommon , x);
+            }
+        }
+
+        //agar nothing is common got -1 :)
+        return (miniCommon == INT_MAX) ? -1 : miniCommon;
+    }
+};
