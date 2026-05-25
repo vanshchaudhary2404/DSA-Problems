980273067ba02110cb85285da1a4c54bf7b68bbd
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-05-25 12:21:46 +0530

    Create 2657. Find the Prefix Common Array of Two Arrays.cpp

---
+++ Daily Challenge/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size(), count = 0;
+        vector<int> res(n);
+        bitset<51> seen;
+
+        for (int i = 0; i < n; i++) {
+            count += seen[A[i]];
+            seen.set(A[i]);
+
+            count += seen[B[i]];
+            seen.set(B[i]);
+
+            res[i] = count;
+        }
+
+        return res;
+    }
+};
