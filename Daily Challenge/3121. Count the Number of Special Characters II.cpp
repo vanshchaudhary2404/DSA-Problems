a287068523e984d3e4efc6d4960ab8ad065e5b25
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-05-26 14:40:59 +0530

    Create 3121. Count the Number of Special Characters II.cpp

---
+++ Daily Challenge/3121. Count the Number of Special Characters II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+//t.c = o(n+26)
+//s.c = o(26*2)
+    int numberOfSpecialChars(string word) {
+        vector<int> lower(26,-1) , upper(26,-1);
+        for(int i=0; i<word.size(); i++){
+            char ch = word[i];
+            if(islower(ch)){
+                lower[ch-'a'] = i;
+            }else if(upper[ch-'A'] == -1) upper[ch-'A'] = i;
+        }
+
+        int ans =0;
+        for(int ch =0; ch<26; ch++){
+            if(lower[ch] != -1 && upper[ch] != -1 && upper[ch] > lower[ch]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
