ed0c631b328aea94509e32e6ef9d44da5fd95007
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-06-23 17:34:32 +0530

    Create 3699. Number of ZigZag Arrays I.cpp

---
+++ Daily Challenge/3699. Number of ZigZag Arrays I.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+    int zigZagArrays(int n, int l, int r) {
+        int m = r-l +1;
+
+        vector<int> dp(m , 1);
+
+        for(int len =2; len<=n; len++){
+            reverse(dp.begin() , dp.end());
+
+            long long pref =0;
+
+            for(int i=0; i<m; i++){
+                int old = dp[i];
+
+                dp[i] = pref;
+
+                pref = (pref + old)% MOD;
+            }
+        }    
+
+        long long  ans =0;
+        for(int x: dp){
+            ans = (ans + x ) % MOD;
+        }
+
+        return (ans * 2) % MOD;
+    }
+};
