3aba76ba9c907899b790c552ef460ae6798b455b
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-07-01 00:49:19 +0530

    Create 1846. Maximum Element After Decreasing and Rearranging.cpp

---
+++ Daily Challenge/1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(begin(arr) , end(arr));
+        int n = arr.size();
+
+        arr[0] = 1;
+        for(int i=1; i<n; i++){
+            arr[i] = min(arr[i] , arr[i-1]+1);
+        }
+
+        return arr[n-1];
+    }
+};
