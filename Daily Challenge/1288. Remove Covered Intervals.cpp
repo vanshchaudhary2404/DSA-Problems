d54cd98f5d7f35ee514bb9aa2d476e7052ab24c9
Vansh Kumar <vanshchaudhary2404@gmail.com>
2026-07-07 01:25:08 +0530

    Create 1288. Remove Covered Intervals.cpp

---
+++ Daily Challenge/1288. Remove Covered Intervals.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        /*
+        vector<vector<int>> result;
+        int n = intervals.size();
+
+        auto lambda = [](vector<int>& vec1 , vector<int>& vec2){
+            if(vec1[0] == vec2[0]){
+                return vec1[1] > vec2[1];
+            }
+            return vec1[0] < vec2[0];
+        };
+
+        sort( begin(intervals) , end(intervals) , lambda);
+
+        result.push_back(intervals[0]);
+        for(int i=1; i<n; i++){
+            if( result.back()[0] <= intervals[i][0] && result.back()[1] >= intervals[i][1]){
+                continue;
+            }
+
+            result.push_back(intervals[i]);
+        }
+
+        return result.size();
+        */
+
+
+        //Approach 2 -> remove this  result.back()[0] <= intervals[i][0] because it is always true
+        /*vector<vector<int>> result;
+        int n = intervals.size();
+
+        auto lambda = [](vector<int>& vec1 , vector<int>& vec2){
+            if(vec1[0] == vec2[0]){
+                return vec1[1] > vec2[1];
+            }
+            return vec1[0] < vec2[0];
+        };
+
+        sort( begin(intervals) , end(intervals) , lambda);
+
+        result.push_back(intervals[0]);
+        for(int i=1; i<n; i++){
+            if(result.back()[1] >= intervals[i][1]){
+                continue;
+            }
+
+            result.push_back(intervals[i]);
+        }
+
+        return result.size();
+        */
+
+        //Approach 3 using variable lastIntervalEnd instead of vectro<vector<int>> result;
+        int n = intervals.size();
+
+        auto lambda = [](vector<int>& vec1 , vector<int>& vec2){
+            if(vec1[0] == vec2[0]){
+                return vec1[1] > vec2[1];
+            }
+            return vec1[0] < vec2[0];
+        };
+
+        sort( begin(intervals) , end(intervals) , lambda);
+
+        int lastIntervalKaEnd = intervals[0][1];
+        int count =1;
+        for(int i=1; i<n; i++){
+            if( lastIntervalKaEnd >= intervals[i][1]){
+                continue;
+            }
+
+            lastIntervalKaEnd = intervals[i][1];
+            count++;
+        }
+
+        return count;
+    }
+
+};
